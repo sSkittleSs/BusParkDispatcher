ad1e9aece0da097b534d13e219bb5eb9cc1dc78e
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-05 18:29:12 +0300

    Добавлена команда для перемещения главного окна по экрану. Добавлена возможность изменения размера окна.

---
+++ BusParkDispatcher/Commands/DragWindowCommand.cs
@@ -0,0 +1,18 @@
+﻿using BusParkDispatcher.Commands.Base;
+using BusParkDispatcher.Models;
+using BusParkDispatcher.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.Commands
+{
+    class DragWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => MainWindowViewModel.MainWindow.DragMove();
+    }
+}
