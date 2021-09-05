042aad6e6bfd277269497c4d4cbe337d6c57dc33
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-05 18:08:44 +0300

    Добавлены основные команды управления приложением.

---
+++ BusParkDispatcher/Commands/OpenHelpCommand.cs
@@ -0,0 +1,30 @@
+﻿using BusParkDispatcher.Commands.Base;
+using BusParkDispatcher.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.Commands
+{
+    class OpenHelpCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\Справка\\index.htm"))
+            {
+                Process.Start(Directory.GetCurrentDirectory() + "\\Справка\\index.htm");
+                NotificationManager.ShowInformation("Справочная система открывается, ожидайте...");
+            }
+            else
+            {
+                NotificationManager.ShowError("Невозможно запустить справочную систему, поскольку она отсутствует или повреждена.");
+            }
+        }
+    }
+}
