7159ded9dda93033288514389c2bfa55f120c2b4
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-08 10:30:19 +0300

    Разработан универсальный класс для работы с моделями базы данных.

---
+++ BusParkDispatcher/Infrastructure/DbTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.Infrastructure
+{
+    public class DbTable
+    {
+        public virtual bool IsSearchable(string value)
+        {
+            Type type = GetType();
+            PropertyInfo[] props = type.GetProperties();
+            foreach (PropertyInfo prop in props)
+            {
+                object val = prop.GetValue(this);
+                if (!val.GetType().IsPrimitive && !(val is string))
+                {
+                    continue;
+                }
+                string stringVal = Convert.ToString(val);
+                if (stringVal.ToLower().Contains(value.ToLower()))
+                {
+                    Debug.WriteLine(stringVal);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
