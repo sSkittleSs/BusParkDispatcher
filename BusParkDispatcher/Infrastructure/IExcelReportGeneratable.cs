b4766023444395ea38aa5db18cdcdf09d5e1b55b
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-13 14:21:55 +0300

    Добавлен интерфейс взаимодействия с MS Excel для создания отчетов.

---
+++ BusParkDispatcher/Infrastructure/IExcelReportGeneratable.cs
@@ -0,0 +1,16 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.Infrastructure
+{
+    interface IExcelReportGeneratable
+    {
+        bool WriteToNewExcelFile(string path);
+
+        ExcelPackage AddNewSheet(ExcelPackage package, string sheetName = default);
+    }
+}
