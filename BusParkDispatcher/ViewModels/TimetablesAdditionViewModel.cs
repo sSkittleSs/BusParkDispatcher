af4c6dff03920b543aae9702fac2f16fc520eba3
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-19 23:16:29 +0300

    Добавлено представление и модель-представление для добавления расписаний в базу данных.

---
+++ BusParkDispatcher/ViewModels/TimetablesAdditionViewModel.cs
@@ -0,0 +1,49 @@
+﻿using BusParkDispatcher.Commands.Base;
+using BusParkDispatcher.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.ViewModels
+{
+    class TimetablesAdditionViewModel : ObservableObject
+    {
+        #region Fields
+        private DateTime дата;
+        private bool являетсяВыходным;
+        #endregion
+
+        #region Properties
+        public DateTime Дата
+        {
+            set => SetProperty(ref дата, value);
+            get => дата;
+        }
+
+        public bool ЯвляетсяВыходным
+        {
+            set => SetProperty(ref являетсяВыходным, value);
+            get => являетсяВыходным;
+        }
+
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Commands / Methods
+        public DelegateCommand Add => new DelegateCommand((obj) =>
+        {
+            MainWindowViewModel.Database.Расписания.Add(new Models.Database.Расписания() { Дата = Дата, ЯвляетсяВыходным = ЯвляетсяВыходным });
+
+            if (obj is AdditionalWindow window)
+            {
+                window.DialogResult = true;
+                window.Close();
+            }
+        });
+        #endregion
+    }
+}
