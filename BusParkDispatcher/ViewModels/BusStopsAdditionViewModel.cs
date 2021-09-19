d480bdfee49f02563bf30fd8718274b132be06c7
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-19 21:12:21 +0300

    Добавлено представление и модель-представление добавления остановок в базу данных.

---
+++ BusParkDispatcher/ViewModels/BusStopsAdditionViewModel.cs
@@ -0,0 +1,56 @@
+﻿using BusParkDispatcher.Commands.Base;
+using BusParkDispatcher.Views.Windows;
+
+namespace BusParkDispatcher.ViewModels
+{
+    class BusStopsAdditionViewModel : ObservableObject
+    {
+        #region Fields
+        private string название;
+        private string описание;
+        #endregion
+
+        #region Properties
+        public string Название
+        {
+            set
+            {
+                if (value.Length > 30)
+                    return;
+
+                SetProperty(ref название, value);
+            }
+            get => название;
+        }
+
+        public string Описание
+        {
+            set
+            {
+                if (value.Length > 150)
+                    return;
+
+                SetProperty(ref описание, value);
+            }
+            get => описание;
+        }
+
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Commands / Methods
+        public DelegateCommand Add => new DelegateCommand((obj) =>
+        {
+            MainWindowViewModel.Database.Остановки.Add(new Models.Database.Остановки() { Название = Название, Описание = Описание });
+
+            if (obj is AdditionalWindow window)
+            {
+                window.DialogResult = true;
+                window.Close();
+            }
+        });
+        #endregion
+    }
+}
---
+++ BusParkDispatcher/Views/BusStopsAdditionView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BusParkDispatcher.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для BusStopsAdditionView.xaml
+    /// </summary>
+    public partial class BusStopsAdditionView : UserControl
+    {
+        public BusStopsAdditionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
