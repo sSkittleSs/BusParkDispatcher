c93d3e4a0ba6e9ceb291338aa9584d9d1e8b3be3
sSkittleSs <anton.bril.blr@gmail.com>
2021-09-26 16:39:00 +0300

    Добавлено окно для создания отчета об отработанном времени.

---
+++ BusParkDispatcher/ViewModels/HoursInDriveReportAdditionalViewModel.cs
@@ -0,0 +1,90 @@
+﻿using BusParkDispatcher.Commands.Base;
+using BusParkDispatcher.Infrastructure;
+using BusParkDispatcher.Models;
+using BusParkDispatcher.Models.Database;
+using BusParkDispatcher.Views.Windows;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusParkDispatcher.ViewModels
+{
+    class HoursInDriveReportAdditionalViewModel : ObservableObject
+    {
+        #region Fields
+        private int кодВодителя;
+        private Водители selectedDriver;
+        #endregion
+
+        #region Properties
+        public int КодВодителя
+        {
+            set => SetProperty(ref кодВодителя, value);
+            get => кодВодителя;
+        }
+
+        public Водители SelectedDriver
+        {
+            set
+            {
+                SetProperty(ref selectedDriver, value);
+                КодВодителя = SelectedDriver?.КодВодителя ?? 0;
+
+                if (SelectedDriver.Автобусы.Count == 0)
+                    NotificationManager.ShowWarning($"Водителю ({SelectedDriver.ФИО}) не присвоен автобус.");
+            }
+            get => selectedDriver;
+        }
+
+        public ICollection<Водители> Водители { set; get; }
+        #endregion
+
+        #region Constructors
+        public HoursInDriveReportAdditionalViewModel()
+        {
+            Водители = MainWindowViewModel.Database.Водители.Local;
+        }
+        #endregion
+
+        #region Commands / Methods
+        public DelegateCommand CreateReport => new DelegateCommand((obj) =>
+        {
+            try
+            {
+                ReportsViewModel.SaveReportToExcelFile(new HoursInDriveReport(КодВодителя));
+
+                if (obj is AdditionalWindow window)
+                {
+                    window.DialogResult = true;
+                    window.Close();
+                }
+            }
+            catch (Exception e) { NotificationManager.ShowError(e.Message); }
+
+        });
+
+        public DelegateCommand CreateReportForAllDrivers => new DelegateCommand((obj) =>
+        {
+            try
+            {
+                var excelPackage = new ExcelPackage();
+                foreach (var item in Водители)
+                    excelPackage = new HoursInDriveReport(item.КодВодителя).AddNewSheet(excelPackage, item.ФИО);
+
+                ReportsViewModel.SaveExcelPackage(excelPackage);
+
+                if (obj is AdditionalWindow window)
+                {
+                    window.DialogResult = true;
+                    window.Close();
+                }
+            }
+            catch (Exception e) { NotificationManager.ShowError(e.Message); }
+
+        });
+        #endregion
+    }
+}
